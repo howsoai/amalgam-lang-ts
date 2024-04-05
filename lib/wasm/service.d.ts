452458fb5f462ed42e90f6f9fbd794409286a125
Lance Gliser <lance.gliser@howso.com>
2024-04-05 14:27:28 -0500

    Removed lib from source control

--- lib/wasm/service.d.ts
+++
@@ -1,6 +0,0 @@
-import type { Amalgam } from "../api";
-import type { AmalgamEmscriptenModule } from "./runtime";
-import { AmalgamWorkerService, type Request } from "../worker/index";
-export declare class AmalgamWasmService extends AmalgamWorkerService<AmalgamEmscriptenModule> {
-    protected handle(amlg: Amalgam<AmalgamEmscriptenModule>, request: Request, channel: MessagePort): Promise<void>;
-}
