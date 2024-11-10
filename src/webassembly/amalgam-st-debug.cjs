716af227dc636778924116abc76e9aac968b6d42
howso-automation <support@howso.com>
2024-11-10 15:43:52 +0000

    Automated Amalgam version bump

--- src/webassembly/amalgam-st-debug.cjs
+++ src/webassembly/amalgam-st-debug.cjs
@@ -62,7 +62,7 @@ if (ENVIRONMENT_IS_NODE) {
 
 // --pre-jses are emitted after the Module integration code, so that they can
 // refer to Module (if they choose; they can also define Module)
-// include: /tmp/tmpk4jxufby.js
+// include: /tmp/tmpgzow9w9p.js
 
   if (!Module['expectedDataFileDownloads']) {
     Module['expectedDataFileDownloads'] = 0;
@@ -244,21 +244,21 @@ Module['FS_createPath']("/", "tzdata", true, true);
 
   })();
 
-// end include: /tmp/tmpk4jxufby.js
-// include: /tmp/tmp_ealjgx4.js
+// end include: /tmp/tmpgzow9w9p.js
+// include: /tmp/tmp_hqq8xdq.js
 
     // All the pre-js content up to here must remain later on, we need to run
     // it.
     if (Module['$ww'] || (typeof ENVIRONMENT_IS_PTHREAD != 'undefined' && ENVIRONMENT_IS_PTHREAD)) Module['preRun'] = [];
     var necessaryPreJSTasks = Module['preRun'].slice();
-  // end include: /tmp/tmp_ealjgx4.js
-// include: /tmp/tmpe1bcykkr.js
+  // end include: /tmp/tmp_hqq8xdq.js
+// include: /tmp/tmpeeyhbvbu.js
 
     if (!Module['preRun']) throw 'Module.preRun should exist because file support used it; did a pre-js delete it?';
     necessaryPreJSTasks.forEach((task) => {
       if (Module['preRun'].indexOf(task) < 0) throw 'All preRun tasks that exist before user pre-js code should remain after; did you replace Module or modify Module.preRun?';
     });
-  // end include: /tmp/tmpe1bcykkr.js
+  // end include: /tmp/tmpeeyhbvbu.js
 
 
 // Sometimes an existing Module object exists with properties
