df04bccc89131caa315d519709a2e7d64b49e06c
Matt Fulp <8397318+fulpm@users.noreply.github.com>
2025-11-07 13:16:46 -0500

    24695: Support load/store sub-entities, MAJOR

--- src/wasm/runtime.ts
+++ src/wasm/runtime.ts
@@ -5,13 +5,19 @@ import AmalgamDebugRuntime from "../webassembly/amalgam-st-debug.cjs";
 
 export type { AmalgamOptions } from "../api";
 
+export type CharPtrArray = { basePtr: bigint; ptrs: bigint[]; size: bigint; free: () => void };
+
 export interface AmalgamEmscriptenModule extends EmscriptenModule, AmalgamModule {
   cwrap: typeof cwrap;
   ccall: typeof ccall;
   UTF8ToString: typeof UTF8ToString;
+  stringToUTF8: typeof stringToUTF8;
+  lengthBytesUTF8: typeof lengthBytesUTF8;
   getValue: typeof getValue;
   setValue: typeof setValue;
   pointerToString: (ptr: number | bigint) => string;
+  stringToPointer: (value: string) => bigint;
+  allocCharPtrArray: (values: string[] | null) => CharPtrArray;
   FS: typeof FS;
   mainScriptUrlOrBlob: string | URL; // Used to manually set url for multithreaded workers (amalgam-mt.cjs)
 }
@@ -55,10 +61,59 @@ function assignRuntimeMethods(amlg: AmalgamEmscriptenModule) {
     }
   };
 
-  amlg.loadEntity = function (...args) {
+  amlg.stringToPointer = function (value: string): bigint {
+    // Convert UTF-8 string to pointer
+    const n = amlg.lengthBytesUTF8(value) + 1;
+    const ptr = amlg._malloc(n);
+    amlg.stringToUTF8(value, ptr, n);
+    return BigInt(ptr);
+  };
+
+  amlg.allocCharPtrArray = function (values: string[] | null): CharPtrArray {
+    if (values == null) {
+      // No array to allocate
+      return { basePtr: 0n, ptrs: [], size: 0n, free: () => {} };
+    }
+    if (values.length == 0) {
+      // Allocate empty array
+      const basePtr = BigInt(amlg._malloc(8));
+      const free = () => {
+        amlg._free(basePtr as unknown as number);
+      };
+      return { basePtr, ptrs: [], size: 0n, free };
+    } else {
+      // Allocate array with values
+      const ptrs = values.map(amlg.stringToPointer);
+      const basePtr = BigInt(amlg._malloc(ptrs.length * 8));
+      amlg.HEAPU64.set(ptrs, Number(basePtr >> 3n));
+
+      const free = () => {
+        ptrs.forEach((p) => amlg._free(p as unknown as number));
+        amlg._free(basePtr as unknown as number);
+      };
+      return { basePtr, ptrs, size: BigInt(ptrs.length), free };
+    }
+  };
+
+  amlg.loadEntity = function (
+    handle: string,
+    filePath: string,
+    fileType: string,
+    persistent: boolean,
+    fileParams: string,
+    writeLog: string,
+    printLog: string,
+    entityPath: string[] | null,
+  ) {
     // Since the return value is a struct, we must preallocate a pointer for it and pass it in
-    // 1 byte (loaded) + 7 bytes (padding) + 8 bytes (message) + 8 bytes (version)
-    const structPtr = amlg._malloc(24);
+    // bytes: 1 (loaded) + 7 (padding) + 8 (message) + 8 (version) + 8 (entity_path) + 8 (entity_path_len)
+    const structPtr = amlg._malloc(40);
+    const {
+      basePtr: entityPathInputPtr,
+      size: entityPathInputLen,
+      free: freeEntityPathInput,
+    } = amlg.allocCharPtrArray(entityPath);
+
     try {
       amlg.ccall(
         "LoadEntity",
@@ -72,18 +127,44 @@ function assignRuntimeMethods(amlg: AmalgamEmscriptenModule) {
           "string", // json_file_params
           "string", // write_log
           "string", // print_log
+          "number", // entity_path pointer
+          "number", // entity_path length
         ],
-        [BigInt(structPtr) as unknown as number, ...args],
+        [
+          BigInt(structPtr) as unknown as number,
+          handle,
+          filePath,
+          fileType,
+          persistent,
+          fileParams,
+          writeLog,
+          printLog,
+          entityPathInputPtr as unknown as number,
+          entityPathInputLen as unknown as number,
+        ],
       );
       const messagePtr = amlg.getValue(structPtr + 8, "i64");
       const versionPtr = amlg.getValue(structPtr + 16, "i64");
+      const entityPathPtr = amlg.getValue(structPtr + 24, "i64");
+      const entityPathLen = amlg.getValue(structPtr + 32, "i64");
+
+      const entityPath: string[] = [];
+      for (let i = 0; i < entityPathLen; i++) {
+        const indexPtr = BigInt(entityPathPtr) + BigInt(i * 8);
+        const valuePtr = amlg.getValue(Number(indexPtr), "i64");
+        entityPath.push(amlg.pointerToString(Number(valuePtr)));
+      }
+      amlg._free(entityPathPtr);
+
       return {
         loaded: Boolean(amlg.getValue(structPtr, "i8")),
         message: amlg.pointerToString(messagePtr),
         version: amlg.pointerToString(versionPtr),
+        entityPath,
       };
     } finally {
       amlg._free(structPtr);
+      freeEntityPathInput();
     }
   };
 
@@ -108,8 +189,8 @@ function assignRuntimeMethods(amlg: AmalgamEmscriptenModule) {
 
   amlg.verifyEntity = function (...args) {
     // Since the return value is a struct, we must preallocate a pointer for it and pass it in
-    // 1 byte (loaded) + 7 bytes (padding) + 8 bytes (message) + 8 bytes (version)
-    const structPtr = amlg._malloc(24);
+    // bytes: 1 (loaded) + 7 (padding) + 8 (message) + 8 (version) + 8 (entity_path) + 8 (entity_path_len)
+    const structPtr = amlg._malloc(40);
     try {
       amlg.ccall(
         "VerifyEntity",
@@ -122,23 +203,64 @@ function assignRuntimeMethods(amlg: AmalgamEmscriptenModule) {
       );
       const messagePtr = amlg.getValue(structPtr + 8, "i64");
       const versionPtr = amlg.getValue(structPtr + 16, "i64");
+      const entityPathPtr = amlg.getValue(structPtr + 24, "i64");
+      const entityPathLen = amlg.getValue(structPtr + 32, "i64");
+
+      const entityPath: string[] = [];
+      for (let i = 0; i < entityPathLen; i++) {
+        const indexPtr = BigInt(entityPathPtr) + BigInt(i * 8);
+        const valuePtr = amlg.getValue(Number(indexPtr), "i64");
+        entityPath.push(amlg.pointerToString(Number(valuePtr)));
+      }
+      amlg._free(entityPathPtr);
+
       return {
         loaded: Boolean(amlg.getValue(structPtr, "i8")),
         message: amlg.pointerToString(messagePtr),
         version: amlg.pointerToString(versionPtr),
+        entityPath,
       };
     } finally {
       amlg._free(structPtr);
     }
   };
 
-  amlg.storeEntity = amlg.cwrap("StoreEntity", null, [
-    "string", // handle
-    "string", // path
-    "string", // file_type
-    "boolean", // persistent
-    "string", // json_file_params
-  ]);
+  amlg.storeEntity = function (
+    handle: string,
+    filePath: string,
+    fileType: string,
+    persistent: boolean,
+    fileParams: string,
+    entityPath: string[] | null,
+  ): void {
+    const { basePtr: entityPathPtr, size: entityPathLen, free: freeEntityPath } = amlg.allocCharPtrArray(entityPath);
+    try {
+      amlg.ccall(
+        "StoreEntity",
+        null,
+        [
+          "string", // handle
+          "string", // path
+          "string", // file_type
+          "boolean", // persistent
+          "string", // json_file_params
+          "number", // entity_path pointer
+          "number", // entity_path length
+        ],
+        [
+          handle,
+          filePath,
+          fileType,
+          persistent,
+          fileParams,
+          entityPathPtr as unknown as number,
+          entityPathLen as unknown as number,
+        ],
+      );
+    } finally {
+      freeEntityPath();
+    }
+  };
   amlg.executeEntity = amlg.cwrap("ExecuteEntity", null, ["string", "string"]);
   amlg.executeEntityJson = function (...args) {
     const ptr = amlg.ccall("ExecuteEntityJsonPtr", "number", ["string", "string", "string"], args);
